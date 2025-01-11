78ecb439ff52ac1b063a9a938ece9df8d71a3a3e
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-01-11 10:51:31 +0100

    ui ist jetzt cool

--- ui/ui.gd
+++ ui/ui.gd
@@ -1,7 +1,21 @@
 extends CanvasLayer
 
 @export var fruit_label: Label
+var fruits: int = 0
 
-func change_fruit_label(text):
-	fruit_label.text = "Fruits needed: " + text
+func _ready() -> void:
+	fruit_label.text = "Fruits: 0"
+	SignalBus.fruit_eaten.connect(update_fruit_label)
+
+func update_fruit_label():
+	fruits = fruits + 1
+	fruit_label.text = "Fruits: " + str(fruits)
 	fruit_label.show()
+
+
+func _on_start_button_pressed() -> void:
+	$MarginContainer.hide()
+
+
+func _on_exit_button_pressed() -> void:
+	get_tree().quit()
