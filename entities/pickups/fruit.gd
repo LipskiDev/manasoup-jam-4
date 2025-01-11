bcf792a7e63045a88e22b91f503f08396087be09
HPLaptop <Hans.Kraschewski@mailbox.tu-dresden.de>
2025-01-11 09:36:29 +0100

    idfk

--- entities/pickups/fruit.gd
+++ entities/pickups/fruit.gd
@@ -18,3 +18,8 @@ func _process(_delta: float) -> void:
 func _on_area_entered(area: Area2D) -> void:
 	gets_eaten.emit()
 	queue_free()
+
+
+func _on_body_entered(body: Node2D) -> void:
+	gets_eaten.emit()
+	queue_free()
