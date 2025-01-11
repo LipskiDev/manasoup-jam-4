57063d6a4570d804f2d5deba45cc1401b1fcdee6
Göndyr <jacob.schlachte@gmail.com>
2025-01-11 13:17:15 +0100

    abstract level object

--- global/signal_bus.gd
+++ global/signal_bus.gd
@@ -4,6 +4,7 @@ signal fruit_eaten
 
 signal trapdoor_entered
 signal current_weight(weight)
+signal change_level(name: String) # just the name e.g. "test_level"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
