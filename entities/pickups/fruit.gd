d74f3cc7163a49ef1ca55416060e112ce591257f
HPLaptop <Hans.Kraschewski@mailbox.tu-dresden.de>
2025-01-11 11:47:38 +0100

    working trapdoor

--- entities/pickups/fruit.gd
+++ entities/pickups/fruit.gd
@@ -1,7 +1,6 @@
 class_name Fruit extends Pickup
 
 var frames
-signal gets_eaten
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,12 +12,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
-
-
-func _on_area_entered(area: Area2D) -> void:
-	gets_eaten.emit()
-	queue_free()
-
 
 func _on_body_entered(body: Node2D) -> void:
 	SignalBus.fruit_eaten.emit()
