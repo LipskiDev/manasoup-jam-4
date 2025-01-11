b1cdabf25a5493e2391899da9b5774ecea85acf6
HPLaptop <Hans.Kraschewski@mailbox.tu-dresden.de>
2025-01-11 15:41:29 +0100

    finish working

---
+++ entities/pickups/finish.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@export var fruit_goal = 1
+var stepped_on = false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	SignalBus.current_weight.connect(_on_current_weight)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	stepped_on = true
+	SignalBus.finish_entered.emit()
+	
+func _on_body_exited(body: Node2D) -> void:
+	stepped_on = false
+	
+func _on_current_weight(weight):
+	if weight >= fruit_goal && stepped_on == true:
+		print("verpupp")
+		$Sprite2D.region_rect.position.x = 64
+		#SignalBus.change_level.emit("parallax_background")
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -25,6 +25,7 @@ func _init() -> void:
 func _ready() -> void:
 	SignalBus.fruit_eaten.connect(_on_fruit_eaten)
 	SignalBus.trapdoor_entered.connect(_on_trapdoor_entered)
+	SignalBus.finish_entered.connect(_on_finish_entered)
 	sprite = stage0
 	sprite.show()
 
@@ -106,3 +107,7 @@ func _on_fruit_eaten():
 	
 func _on_trapdoor_entered():
 	SignalBus.weight_on_trapdoor.emit(current_fruits)
+	
+func _on_finish_entered():
+	SignalBus.current_weight.emit(current_fruits)
+		
