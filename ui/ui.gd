1c78189d2b44dca78bf230a24dac619f4e6c783a
Frederik Hoheisel <frederik.hoheisel@mailbox.tu-dresden.de>
2025-01-12 14:42:32 +0100

    level updates

--- ui/ui.gd
+++ ui/ui.gd
@@ -1,16 +1,8 @@
 extends CanvasLayer
 
-@export var fruit_label: Label
-
 func _ready() -> void:
-	fruit_label.text = "Fruits: 0"
-	SignalBus.current_weight.connect(update_fruit_label)
-
-func update_fruit_label(text):
-	fruit_label.text = "Fruits: " + str(text)
-	fruit_label.show()
-
-
+	pass
+	
 func _on_start_button_pressed() -> void:
 	$MarginContainer.hide()
 
