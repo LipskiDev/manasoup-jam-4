d30d2bef9f3cdda070d112e50df6777130888d18
LipskiDev <max.lipski@hotmail.com>
2025-01-12 14:48:12 +0100

    Added Level and Sprites

---
+++ level/message_shower.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	body_entered.connect(show_message)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+	
+func show_message(body: Node2D):
+	print("workds")
+	$Sprite2D.show()
+	pass
